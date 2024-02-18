99ca48947c4c2cc5609af1756ada34095c53212f
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2024-02-17 23:40:24 -0600

    Removed Firebase
    Removed firebase to test out how Kinde works

--- app/_components/logInOut.jsx
+++ app/_components/logInOut.jsx
@@ -1,49 +1,26 @@
 'use client'
 import { useState, useEffect } from "react";
 import { useRouter } from 'next/navigation';
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
-import app from '../firebase/config';
 
 export default function LogInOut() {
 
     const [user, setUser] = useState(null);
     const router = useRouter();
 
-    useEffect(() => {
-        const auth = getAuth(app);
-        const unsubscribe = auth.onAuthStateChanged((user) => {
-            if (user) {
-                setUser(user);
-            } else {
-                setUser(null);
-            };
-        });
+    const doA= async () => {
+        return "blah"
+    }
 
-        return () => unsubscribe();
-    
-    }, []);
-
-    const signInWithGoogle = async () => {
-        const auth = getAuth(app);
-        const provider = new GoogleAuthProvider();
-        try {
-            let theUser = await signInWithPopup(auth, provider);
-            console.log(theUser);
-        } catch(e) {
-            console.error('Error Signing in with Google',e);
-        };
-    };
-
-    const logOut = async () => {
+    const doB = async () => {
         return "blah"
     }
 
     return (
         <div className="flex flex-col items-center justify-around space-y-4">
             {user ? (
-                <button className="bg-blue-300 p-2" onClick={logOut}>Log Out</button>       
+                <button className="bg-blue-300 p-2" onClick={doB}>Log Out</button>       
                 ) : (
-                <button className="bg-blue-300 p-2" onClick={signInWithGoogle}>Log In</button>
+                <button className="bg-blue-300 p-2" onClick={doA}>Log In</button>
             )}
         </div>
     );
