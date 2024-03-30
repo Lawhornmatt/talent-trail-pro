e0c5ee2db9f56c4b48588044bd9c1ca14b57ee32
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2024-03-30 14:29:15 -0500

    Settings Readability
    Copied over the same class names I applied to the Add page

--- app/settings/page.jsx
+++ app/settings/page.jsx
@@ -2,10 +2,10 @@ import Nav from "../_components/Nav";
 
 export default function Settings() {
     return (
-      <div>
-         <Nav/>
-      <h1> Hello Settings </h1>
-      </div>
+        <main className="flex flex-col items-center justify-start p-4 space-y-6 h-screen">
+            <Nav/>
+            <h1> Hello Settings </h1>
+        </main>
     );
   }
   
