c89c06d24de54dc84846015d6ec1c7509944e0fd
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2024-03-30 14:20:22 -0500

    Readability
    Didnt change any functionality but added some CSS classes to make seeing the Nav and the Add Job forms a little easier to read.

--- app/_components/Nav.jsx
+++ app/_components/Nav.jsx
@@ -4,22 +4,20 @@ import Link from "next/link";
 
 export default function Nav() {
    return (
-    <div>
-        <ul>
-            <li>
-                <Link href="/"> Login </Link>
-            </li>
-            <li>
-                <Link href="/home"> Home </Link>
-            </li>
-            <li>
-                <Link href="/add">Add</Link> 
-            </li>
-            <li>
-                <Link href="/settings">Settings</Link> 
-            </li>
-        </ul>
-    </div>
+    <ul className="space-y-2">
+        <li>
+            <Link href="/" className="bg-blue-300 p-1"> Login </Link>
+        </li>
+        <li>
+            <Link href="/home" className="bg-blue-300 p-1"> Home </Link>
+        </li>
+        <li>
+            <Link href="/add" className="bg-blue-300 p-1">Add</Link> 
+        </li>
+        <li>
+            <Link href="/settings" className="bg-blue-300 p-1">Settings</Link> 
+        </li>
+    </ul>
    );
 }
 
--- app/add/page.jsx
+++ app/add/page.jsx
@@ -51,42 +51,42 @@ export default function Add() {
 
   return (
     
-    <div>
+    <main className="flex flex-col items-center justify-start p-4 space-y-6 h-screen">
       <Nav />
       
       <p> Hello Add </p>
 
       {error && <div style={{ color: 'red' }}>{error}</div>}
 
-      <form method="post" onSubmit={handleSubmit}>
-        <label>
+      <form method="post" onSubmit={handleSubmit} className='flex flex-col justify-center items-center w-3/4 space-y-2'>
+        <label className='flex justify-between w-3/5'>
           Company Name
           <textarea name="companyName" rows={1} cols={40} />
         </label>
 
-        <label>
+        <label className='flex justify-between w-3/5'>
           Position
           <textarea name="position" rows={1} cols={40}/>
         </label>
 
-        <label>
+        <label className='flex justify-between w-3/5'>
           Location
           <textarea name="location" rows={1} cols={40} />
         </label>
 
-        <label>
+        <label className='flex justify-between w-3/5'>
           Applied At
           <textarea name="appliedAt" rows={1} cols={40} />
         </label>
 
-        <label>
+        <label className='flex justify-between w-3/5'>
           Date
           <input type='date' />
         </label>
 
-        <button type="submit"> Save </button>
+        <button type="submit" className="bg-blue-300 p-1"> Save </button>
       </form>
 
-    </div>
+    </main>
   );
 }
