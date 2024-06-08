34a12a5fd11f21766a3b244b69de06c6db06a204
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2024-06-08 17:26:49 -0500

    GET SYNCHED
    All of Devante changes pushed into 'mimundoc' branch. Pushing all of mine into 'DevFrontend+MattConnect'. Going to merge 'mimundoc' into main first then my branch.

---
+++ app/_components/Add.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+//placeholder function
+async function addJobApp() {
+    'use server'
+    console.log('On the server');
+    // POST
+  };
+
+export default function Add({ user }) {
+    return (
+        <p>The Add Page</p>
+    );
+}
---
+++ app/_components/Title.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Title() {
+    return (
+        <div className="bg-blue-600 w-full h-1/6 flex justify-center ">
+            <h1>Talent Trail Pro</h1>
+        </div>
+    );
+}
+
